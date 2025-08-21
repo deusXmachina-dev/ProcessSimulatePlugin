8523c3898daf014e44de899e3be96b511ce2bf99
FilipKubis <filip@senseflow.io>
2025-08-21 18:37:44 +0200

    Add project files.

---
+++ TxCommand1/TxHelloWorldCmd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tecnomatix.Engineering;
+
+namespace TxCommand1
+{
+    public class TxHelloWorldCmd : TxButtonCommand
+    {
+        public override string Category => StringTable.Category;
+
+        public override string Name => StringTable.NAME;
+
+        public override void Execute(object cmdParams)
+        {
+            TxRobotTCPForm robotForm = new TxRobotTCPForm();
+            robotForm.Show();
+        }
+    }
+}
---
+++ TxCommand1/TxRobotTCPForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tecnomatix.Engineering.Ui;
+
+namespace TxCommand1
+{
+    public partial class TxRobotTCPForm : TxForm
+    {
+        public TxRobotTCPForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
