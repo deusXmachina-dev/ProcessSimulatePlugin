47486d9889cd782c1f59fe5bb653fbcd24f7abbc
FilipKubis <filip@senseflow.io>
2025-10-03 11:46:02 +0200

    outline the pseudo algorithm in the code

--- TxCommand1/Operations/HeuristicEnergyOptimizer.cs
+++ TxCommand1/Operations/HeuristicEnergyOptimizer.cs
@@ -21,6 +21,19 @@ namespace TxCommand1.Operations
         {
             // TODO: Implement heuristic optimization logic
             throw new System.NotImplementedException();
+
+            // todo: 1. Get all movements (each is a sequence of joint operations)
+            // todo: 2. For each movement calculate total distance travelled and total vertical distance travelled
+            // todo: 3. Sort movements by heuristic score (distance travelled + 2 * vertical distance travelled)
+
+            // todo: 4. start heuristic optimization:
+            // todo: 4.1. for movement in sorted list:
+            // todo: 4.1.1. reduce speed to something around 60% if this does not exceed the limit duration
+            // todo: 4.1.2. if it does exceed the limit duration, reduce speed as much as possible while staying within the limit duration and then break
+
+            // todo: 4.2 for movement in sorted list:
+            // todo: 4.2.1. reduce speed to something around 45% if this does not exceed the limit duration
+            // todo: 4.2.2. if it does exceed the limit duration, reduce speed as much as possible while staying within the limit duration and then break
         }
     }
 }
